3772eae68fc117dc618a73141bd2897f29c75ebd
darialoskutova <darialoskutova04@gmail.com>
2026-08-13 22:07:58 +0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,99 @@
+const topics = {
+    soc1: {
+        title: "Человек и его потребности",
+        lesson: "01",
+        notes: "topic1soc-notes.pdf",
+        test: "https://docs.google.com/forms/d/e/1FAIpQLSfH-PHnaZ1HXc_NYEEPhD8Jj8VsM170FSS79--Rst3fXZuEWA/viewform?usp=sharing&ouid=102491429070271040965"
+    },
+
+    soc2: {
+        title: "Мировоззрение и мышление",
+        lesson: "02",
+        notes: "",
+        test: ""
+    },
+
+    soc3: {
+        title: "Деятельность и способности",
+        lesson: "03",
+        notes: "",
+        test: ""
+    },
+
+    soc4: {
+        title: "Познание. Истина",
+        lesson: "04",
+        notes: "",
+        test: ""
+    },
+
+    soc5: {
+        title: "Общество и его системное строение",
+        lesson: "05",
+        notes: "",
+        test: ""
+    },
+
+    soc6: {
+        title: "Глобализация",
+        lesson: "06",
+        notes: "",
+        test: ""
+    },
+
+    soc7: {
+        title: "Общество и его типологии",
+        lesson: "07",
+        notes: "",
+        test: ""
+    },
+
+    soc8: {
+        title: "Культура. Наука и образование",
+        lesson: "08",
+        notes: "",
+        test: ""
+    },
+
+    soc9: {
+        title: "Культура. Искусство, мораль, религия",
+        lesson: "09",
+        notes: "",
+        test: ""
+    },
+
+    soc10: {
+        title: "Соц. группы. Этнос.",
+        lesson: "10",
+        notes: "",
+        test: ""
+    },
+
+    soc11: {
+        title: "Соц. стратификация и соц. мобильность",
+        lesson: "11",
+        notes: "",
+        test: ""
+    },
+
+    soc12: {
+        title: "Соц. ценности. Соц. конфликт",
+        lesson: "12",
+        notes: "",
+        test: ""
+    },
+
+    soc13: {
+        title: "Социальный контроль. Отклоняющееся поведение",
+        lesson: "13",
+        notes: "",
+        test: ""
+    },
+
+    soc14: {
+        title: "Семья и брак",
+        lesson: "14",
+        notes: "",
+        test: ""
+    }
+};
