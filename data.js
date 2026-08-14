624b2a4bd05de1475f7e3e0cea0514232c474fcf
darialoskutova <darialoskutova04@gmail.com>
2026-08-15 02:42:44 +0500

    Add files via upload

--- data.js
+++ data.js
@@ -1,99 +1,115 @@
 const topics = {
+
     soc1: {
         title: "Человек и его потребности",
         lesson: "01",
-        notes: "topic1soc-notes.pdf",
+        notes: "sociology/1. Человек и его потребности..pdf",
+        homework: "hw-sociology/ДЗ 1. Человек и его потребности.docx",
         test: "https://docs.google.com/forms/d/e/1FAIpQLSfH-PHnaZ1HXc_NYEEPhD8Jj8VsM170FSS79--Rst3fXZuEWA/viewform?usp=sharing&ouid=102491429070271040965"
     },
 
     soc2: {
         title: "Мировоззрение и мышление",
         lesson: "02",
-        notes: "",
-        test: ""
+        notes: "sociology/2. Мировоззрение и мышление.pdf",
+        homework: "hw-sociology/ДЗ 2. Мировоззрение и мышление.docx",
+        test: "https://docs.google.com/forms/d/e/1FAIpQLSdOKvLAJg_lT3Elvr_nMRgUNNwksfcVKKAFqrEhquaWIX_Jgw/viewform?usp=sharing&ouid=102491429070271040965"
     },
 
     soc3: {
         title: "Деятельность и способности",
         lesson: "03",
-        notes: "",
-        test: ""
+        notes: "sociology/3. Деятельность и способности.pdf",
+        homework: "hw-sociology/ДЗ 3. Деятельность и способности.docx",
+        test: "https://docs.google.com/forms/d/e/1FAIpQLScO-H9gECxe77r5PWJNAQYjO5CJA4g0ZDWJ3826NNRyzFUTTA/viewform?usp=sharing&ouid=102491429070271040965"
     },
 
     soc4: {
         title: "Познание. Истина",
         lesson: "04",
-        notes: "",
-        test: ""
+        notes: "sociology/4. Познание. Истина...pdf",
+        homework: "hw-sociology/ДЗ 4. Познание и истина.docx",
+        test: "https://docs.google.com/forms/d/e/1FAIpQLSeruyFMTFVcO-pVeeOCcL3Vk5l1VNzDr5FrC8ZkzXzPJJLgiA/viewform?usp=sharing&ouid=102491429070271040965"
     },
 
     soc5: {
         title: "Общество и его системное строение",
         lesson: "05",
-        notes: "",
-        test: ""
+        notes: "sociology/5. Общество и его системное строение..pdf",
+        homework: "hw-sociology/ДЗ 5. Общество, его системное строение.docx",
+        test: "https://docs.google.com/forms/d/e/1FAIpQLSf9-ajeN2SKjlcEaSroEEhnQ16pKSp6lbsMS4ru8jInCh81ag/viewform?usp=sharing&ouid=102491429070271040965"
     },
 
     soc6: {
         title: "Глобализация",
         lesson: "06",
-        notes: "",
-        test: ""
+        notes: "sociology/6. Глобализация. Глобальные проблемы.pdf",
+        homework: "hw-sociology/ДЗ 6. Глобализация.docx",
+        test: "https://docs.google.com/forms/d/e/1FAIpQLSfyW9-ji8Bv6JjXJifP6lHUc9yfAGMmAVfxxHYbvIW6ue3gxw/viewform?usp=sharing&ouid=102491429070271040965"
     },
 
     soc7: {
         title: "Общество и его типологии",
         lesson: "07",
-        notes: "",
-        test: ""
+        notes: "sociology/7. Типология общества.pdf",
+        homework: "hw-sociology/ДЗ 7. Типология общества.docx",
+        test: "https://docs.google.com/forms/d/e/1FAIpQLSe2qoqTWwHJLLTBQ_8rlcd3YVLhk75mJIAwHrpxBh6_-5x4kg/viewform?usp=sharing&ouid=102491429070271040965"
     },
 
     soc8: {
         title: "Культура. Наука и образование",
         lesson: "08",
-        notes: "",
-        test: ""
+        notes: "sociology/8. Культура (наука и образование).pdf",
+        homework: "hw-sociology/ДЗ 8. Культура (наука и образование).docx",
+        test: "https://docs.google.com/forms/d/e/1FAIpQLSf4_ha2dxsqdsJDKfan9kIN0-cNGWN8xRihA-mzxJh-KaudcA/viewform?usp=sharing&ouid=102491429070271040965"
     },
 
     soc9: {
         title: "Культура. Искусство, мораль, религия",
         lesson: "09",
-        notes: "",
-        test: ""
+        notes: "sociology/9. Культура (искусство, религия, мораль).pdf",
+        homework: "hw-sociology/ДЗ 9. Культура (искусство, мораль, религия) .docx",
+        test: "https://docs.google.com/forms/d/e/1FAIpQLSenKkFI2IwAP9PBlPk4I0lOdoE9vd087DBgVCvlMD41uosvTQ/viewform?usp=sharing&ouid=102491429070271040965"
     },
 
     soc10: {
-        title: "Соц. группы. Этнос.",
+        title: "Соц. стратификация и соц. мобильность",
         lesson: "10",
-        notes: "",
-        test: ""
+        notes: "sociology/10. Соц. стратификация. Соц. мобильность.pdf",
+        homework: "hw-sociology/ДЗ 10. Соц. стратификация. Соц. мобильность.docx",
+        test: "https://docs.google.com/forms/d/e/1FAIpQLSfSPCEPElKaktqqgoqyfrilY_WogE_8mAoNfK23AX1bOV2LyA/viewform?usp=sharing&ouid=102491429070271040965"
     },
 
     soc11: {
-        title: "Соц. стратификация и соц. мобильность",
+        title: "Соц. группы. Этнос.",
         lesson: "11",
-        notes: "",
-        test: ""
+        notes: "sociology/11. Соц. группы. Этнос.pdf",
+        homework: "hw-sociology/ДЗ 11. Соц. группы. Этнос.docx",
+        test: "https://docs.google.com/forms/d/e/1FAIpQLSfCKr4InJZ8g6aLBz7toa0MlL8owPw_agJmVBz_I786irVrIg/viewform?usp=sharing&ouid=102491429070271040965"
     },
 
     soc12: {
         title: "Соц. ценности. Соц. конфликт",
         lesson: "12",
-        notes: "",
-        test: ""
+        notes: "sociology/12. Соц. ценности. Соц. конфликт.pdf",
+        homework: "hw-sociology/ДЗ 12. Соц. ценности. Соц. конфликт.docx",
+        test: "https://docs.google.com/forms/d/e/1FAIpQLSeZQkQFhTKAFNGOpBK4p57d_Imig67ROM4KcJHx7M4EAb_y8A/viewform?usp=sharing&ouid=102491429070271040965"
     },
 
     soc13: {
         title: "Социальный контроль. Отклоняющееся поведение",
         lesson: "13",
-        notes: "",
-        test: ""
+        notes: "sociology/13. Соц. контроль.pdf",
+        homework: "hw-sociology/ДЗ 13. Соц. контроль.docx",
+        test: "https://docs.google.com/forms/d/e/1FAIpQLSe0euVwiQmgAqbgjjvQQ4rmXCrVeomz0rx826T1PyD5Sb6DOg/viewform?usp=sharing&ouid=102491429070271040965"
     },
 
     soc14: {
         title: "Семья и брак",
         lesson: "14",
-        notes: "",
-        test: ""
+        notes: "sociology/14. Семья.pdf",
+        homework: "hw-sociology/ДЗ 14. Семья и брак.docx",
+        test: "https://docs.google.com/forms/d/e/1FAIpQLSd5jhvcESVf1FE7dOoZH0xzm-heBCnQs2HHBWtwHHlKFKLafw/viewform?usp=sharing&ouid=102491429070271040965"
     }
+
 };
