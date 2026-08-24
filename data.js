152fd1d248e552e9bfccebf4693b0599eeafc0a0
darialoskutova <darialoskutova04@gmail.com>
2026-08-24 19:23:18 +0300

    Add files via upload

--- data.js
+++ data.js
@@ -1,11 +1,15 @@
 const topics = {
 
+    // =========================
+    // СОЦИОЛОГИЯ
+    // =========================
+
     soc1: {
         title: "Человек и его потребности",
         lesson: "01",
         notes: "1. Человек и его потребности..pdf",
         homework: "ДЗ 1. Человек и его потребности.docx",
-        test: "https://docs.google.com/forms/d/e/1FAIpQLSfH-PHnaZ1HXc_NYEEPhD8Jj8VsM170FSS79--Rst3fXZuEWA/viewform?usp=sharing&ouid=102491429070271040965"
+        test: "https://docs.google.com/forms/d/e/1FAIpQLSfH-PHnaZ1HXc_NYEEPhD8Jj8VsM170FSS79--Rst3fXZuEWA/viewform"
     },
 
     soc2: {
@@ -13,7 +17,7 @@ const topics = {
         lesson: "02",
         notes: "2. Мировоззрение и мышление.pdf",
         homework: "ДЗ 2. Мировоззрение и мышление.docx",
-        test: "https://docs.google.com/forms/d/e/1FAIpQLSdOKvLAJg_lT3Elvr_nMRgUNNwksfcVKKAFqrEhquaWIX_Jgw/viewform?usp=sharing&ouid=102491429070271040965"
+        test: "https://docs.google.com/forms/d/e/1FAIpQLSdOKvLAJg_lT3Elvr_nMRgUNNwksfcVKKAFqrEhquaWIX_Jgw/viewform"
     },
 
     soc3: {
@@ -21,7 +25,7 @@ const topics = {
         lesson: "03",
         notes: "3. Деятельность и способности.pdf",
         homework: "ДЗ 3. Деятельность и способности.docx",
-        test: "https://docs.google.com/forms/d/e/1FAIpQLScO-H9gECxe77r5PWJNAQYjO5CJA4g0ZDWJ3826NNRyzFUTTA/viewform?usp=sharing&ouid=102491429070271040965"
+        test: "https://docs.google.com/forms/d/e/1FAIpQLScO-H9gECxe77r5PWJNAQYjO5CJA4g0ZDWJ3826NNRyzFUTTA/viewform"
     },
 
     soc4: {
@@ -29,7 +33,7 @@ const topics = {
         lesson: "04",
         notes: "4. Познание. Истина...pdf",
         homework: "ДЗ 4. Познание и истина.docx",
-        test: "https://docs.google.com/forms/d/e/1FAIpQLSeruyFMTFVcO-pVeeOCcL3Vk5l1VNzDr5FrC8ZkzXzPJJLgiA/viewform?usp=sharing&ouid=102491429070271040965"
+        test: "https://docs.google.com/forms/d/e/1FAIpQLSeruyFMTFVcO-pVeeOCcL3Vk5l1VNzDr5FrC8ZkzXzPJJLgiA/viewform"
     },
 
     soc5: {
@@ -37,7 +41,7 @@ const topics = {
         lesson: "05",
         notes: "5. Общество и его системное строение..pdf",
         homework: "ДЗ 5. Общество, его системное строение.docx",
-        test: "https://docs.google.com/forms/d/e/1FAIpQLSf9-ajeN2SKjlcEaSroEEhnQ16pKSp6lbsMS4ru8jInCh81ag/viewform?usp=sharing&ouid=102491429070271040965"
+        test: "https://docs.google.com/forms/d/e/1FAIpQLSf9-ajeN2SKjlcEaSroEEhnQ16pKSp6lbsMS4ru8jInCh81ag/viewform"
     },
 
     soc6: {
@@ -45,7 +49,7 @@ const topics = {
         lesson: "06",
         notes: "6. Глобализация. Глобальные проблемы.pdf",
         homework: "ДЗ 6. Глобализация.docx",
-        test: "https://docs.google.com/forms/d/e/1FAIpQLSfyW9-ji8Bv6JjXJifP6lHUc9yfAGMmAVfxxHYbvIW6ue3gxw/viewform?usp=sharing&ouid=102491429070271040965"
+        test: "https://docs.google.com/forms/d/e/1FAIpQLSfyW9-ji8Bv6JjXJifP6lHUc9yfAGMmAVfxxHYbvIW6ue3gxw/viewform"
     },
 
     soc7: {
@@ -53,7 +57,7 @@ const topics = {
         lesson: "07",
         notes: "7. Типология общества.pdf",
         homework: "ДЗ 7. Типология обществ.docx",
-        test: "https://docs.google.com/forms/d/e/1FAIpQLSe2qoqTWwHJLLTBQ_8rlcd3YVLhk75mJIAwHrpxBh6_-5x4kg/viewform?usp=sharing&ouid=102491429070271040965"
+        test: "https://docs.google.com/forms/d/e/1FAIpQLSe2qoqTWwHJLLTBQ_8rlcd3YVLhk75mJIAwHrpxBh6_-5x4kg/viewform"
     },
 
     soc8: {
@@ -61,7 +65,7 @@ const topics = {
         lesson: "08",
         notes: "8. Культура (наука и образование).pdf",
         homework: "ДЗ 8. Культура (наука и образование).docx",
-        test: "https://docs.google.com/forms/d/e/1FAIpQLSf4_ha2dxsqdsJDKfan9kIN0-cNGWN8xRihA-mzxJh-KaudcA/viewform?usp=sharing&ouid=102491429070271040965"
+        test: "https://docs.google.com/forms/d/e/1FAIpQLSf4_ha2dxsqdsJDKfan9kIN0-cNGWN8xRihA-mzxJh-KaudcA/viewform"
     },
 
     soc9: {
@@ -69,7 +73,7 @@ const topics = {
         lesson: "09",
         notes: "9. Культура (искусство, религия, мораль).pdf",
         homework: "ДЗ 9. Культура (Искусство. Мораль. Религия).docx",
-        test: "https://docs.google.com/forms/d/e/1FAIpQLSenKkFI2IwAP9PBlPk4I0lOdoE9vd087DBgVCvlMD41uosvTQ/viewform?usp=sharing&ouid=102491429070271040965"
+        test: "https://docs.google.com/forms/d/e/1FAIpQLSenKkFI2IwAP9PBlPk4I0lOdoE9vd087DBgVCvlMD41uosvTQ/viewform"
     },
 
     soc10: {
@@ -77,7 +81,7 @@ const topics = {
         lesson: "10",
         notes: "10. Соц. стратификация. Соц. мобильность.pdf",
         homework: "ДЗ 10. Соц. стратификация. Соц. мобильность.docx",
-        test: "https://docs.google.com/forms/d/e/1FAIpQLSfSPCEPElKaktqqgoqyfrilY_WogE_8mAoNfK23AX1bOV2LyA/viewform?usp=sharing&ouid=102491429070271040965"
+        test: "https://docs.google.com/forms/d/e/1FAIpQLSfSPCEPElKaktqqgoqyfrilY_WogE_8mAoNfK23AX1bOV2LyA/viewform"
     },
 
     soc11: {
@@ -85,7 +89,7 @@ const topics = {
         lesson: "11",
         notes: "11. Соц. группы. Этнос.pdf",
         homework: "ДЗ 11. Соц. группы. Этнос.docx",
-        test: "https://docs.google.com/forms/d/e/1FAIpQLSfCKr4InJZ8g6aLBz7toa0MlL8owPw_agJmVBz_I786irVrIg/viewform?usp=sharing&ouid=102491429070271040965"
+        test: "https://docs.google.com/forms/d/e/1FAIpQLSfCKr4InJZ8g6aLBz7toa0MlL8owPw_agJmVBz_I786irVrIg/viewform"
     },
 
     soc12: {
@@ -93,7 +97,7 @@ const topics = {
         lesson: "12",
         notes: "12. Соц. ценности. Соц. конфликт.pdf",
         homework: "ДЗ 12. Соц. ценности. Соц. конфликт.docx",
-        test: "https://docs.google.com/forms/d/e/1FAIpQLSeZQkQFhTKAFNGOpBK4p57d_Imig67ROM4KcJHx7M4EAb_y8A/viewform?usp=sharing&ouid=102491429070271040965"
+        test: "https://docs.google.com/forms/d/e/1FAIpQLSeZQkQFhTKAFNGOpBK4p57d_Imig67ROM4KcJHx7M4EAb_y8A/viewform"
     },
 
     soc13: {
@@ -101,7 +105,7 @@ const topics = {
         lesson: "13",
         notes: "13. Соц. контроль.pdf",
         homework: "ДЗ 13. Соц. контроль.docx",
-        test: "https://docs.google.com/forms/d/e/1FAIpQLSe0euVwiQmgAqbgjjvQQ4rmXCrVeomz0rx826T1PyD5Sb6DOg/viewform?usp=sharing&ouid=102491429070271040965"
+        test: "https://docs.google.com/forms/d/e/1FAIpQLSe0euVwiQmgAqbgjjvQQ4rmXCrVeomz0rx826T1PyD5Sb6DOg/viewform"
     },
 
     soc14: {
@@ -109,7 +113,124 @@ const topics = {
         lesson: "14",
         notes: "14. Семья.pdf",
         homework: "ДЗ 14. Семья и брак.docx",
-        test: "https://docs.google.com/forms/d/e/1FAIpQLSd5jhvcESVf1FE7dOoZH0xzm-heBCnQs2HHBWtwHHlKFKLafw/viewform?usp=sharing&ouid=102491429070271040965"
+        test: "https://docs.google.com/forms/d/e/1FAIpQLSd5jhvcESVf1FE7dOoZH0xzm-heBCnQs2HHBWtwHHlKFKLafw/viewform"
+    },
+
+
+    // =========================
+    // ЭКОНОМИКА
+    // =========================
+
+    eco15: {
+        title: "Экономика как наука и хозяйство",
+        lesson: "15",
+        notes: "15. Экономика как наука и хозяйство.pdf",
+        homework: "ДЗ 15. Экономика как наука и хозяйство.docx",
+        test: "https://docs.google.com/forms/d/e/1FAIpQLSd-a9PIxJBDuhjvDZyoSFINyGm1IdJu4hMFwEkG-ZRCdfURLg/viewform"
+    },
+
+    eco16: {
+        title: "Экономические системы",
+        lesson: "16",
+        notes: "16. Экономические системы.pdf",
+        homework: "ДЗ 16. Экономические системы.docx",
+        test: "https://docs.google.com/forms/d/e/1FAIpQLSer00qeKGXV6NhbDuScKu7J1nBsjl9IDyq4PQIcakBPYHyqKg/viewform"
+    },
+
+    eco17: {
+        title: "Рынок. Спрос и предложение",
+        lesson: "17",
+        notes: "17. Рынок. Спрос и предложение. pdf.pdf",
+        homework: "ДЗ 17. Рынок. Спрос и предложение.docx",
+        test: "https://docs.google.com/forms/d/e/1FAIpQLScBComEkmc1aYdw2GAPy1jmK6nAu01la8iZaln-SD0MwVA8FA/viewform"
+    },
+
+    eco18: {
+        title: "Рынок. Торговля",
+        lesson: "18",
+        notes: "18. Рынок. Торговля.pdf",
+        homework: "ДЗ 18. Рынок. Торговля.docx",
+        test: "https://docs.google.com/forms/d/e/1FAIpQLSdUeyT3hNRAuhoVvLZwO1plsa92XzqTvNDntZN-MM73-vdT2w/viewform"
+    },
+
+    eco19: {
+        title: "Виды рынков. Конкуренция",
+        lesson: "19",
+        notes: "19.Виды рынков. Конкуренция.pdf",
+        homework: "ДЗ 19. Виды рынков. Конкуренция.docx",
+        test: "https://docs.google.com/forms/d/e/1FAIpQLSdK_Asa9-AlKYty6lxnfhZs4_NZuPbUa4_97V5x2SkaZyRw9Q/viewform"
+    },
+
+    eco20: {
+        title: "Государство в рыночной экономике. Мировая экономика",
+        lesson: "20",
+        notes: "20. Государство в рыночной экономике. Мировая экономика. pdf.pdf",
+        homework: "ДЗ 20. Государство в рыночной экономике.docx",
+        test: "https://docs.google.com/forms/d/e/1FAIpQLScX7Mg1wodiANsYpX4YKGe4tKLEBRaqkQ2VcYuxMT35_hwehQ/viewform"
+    },
+
+    eco21: {
+        title: "Государственный бюджет. Собственность",
+        lesson: "21",
+        notes: "21. Государственный бюджет. Собственность. pdf.pdf",
+        homework: "ДЗ 21. Гос. бюджет. Собственность.docx",
+        test: "https://docs.google.com/forms/d/e/1FAIpQLSfGONZV8kJDYTe2o-UdpiMUheHmtmn1rugtvNdkhCdGgasTdA/viewform"
+    },
+
+    eco22: {
+        title: "Финансовые институты. Финансовые услуги",
+        lesson: "22",
+        notes: "22. Финансовые институты. Финансовые услуги. pdf.pdf",
+        homework: "ДЗ 22. Финансовые институты.docx",
+        test: "https://docs.google.com/forms/d/e/1FAIpQLSesNJaGahmofvMxXBNQROew9iiLJb5O9khxapkuhexZP2pEMA/viewform"
+    },
+
+    eco23: {
+        title: "Предпринимательство",
+        lesson: "23",
+        notes: "23. Предпринимательство.pdf",
+        homework: "ДЗ 23. Предпринимательство.docx",
+        test: "https://docs.google.com/forms/d/e/1FAIpQLSce7Ff0Z6UDqH59Fqy4EEbLW2yjdYOhCjwuJlgsHKaKXF8QuA/viewform"
+    },
+
+    eco24: {
+        title: "Деньги. Ценные бумаги",
+        lesson: "24",
+        notes: "24. Деньги. Ценные бумаги.pdf",
+        homework: "ДЗ 24. Деньги. Ценные бумаги.docx",
+        test: "https://docs.google.com/forms/d/e/1FAIpQLSdAzaVXtfiKz-50RXZqfGeonCNzJlCZZD-9LOApaHDWdg7BkQ/viewform"
+    },
+
+    eco25: {
+        title: "Инфляция. Семейный бюджет",
+        lesson: "25",
+        notes: "25. Инфляция. Семейный бюджет.pdf",
+        homework: "ДЗ 25. Инфляция. Семейный бюджет.docx",
+        test: "https://docs.google.com/forms/d/e/1FAIpQLSdYgHiwEocuP7QYTIR7X6Ov1ChytxPhPdJ6EjiEL_4g3Jq8Lw/viewform"
+    },
+
+    eco26: {
+        title: "Рынок труда",
+        lesson: "26",
+        notes: "26. Рынок труда.pdf",
+        homework: "ДЗ 26. Рынок труда .docx",
+        test: "https://docs.google.com/forms/d/e/1FAIpQLSedJY_fC7Hj0KRtrFcV7XvRSapsUuk47Eacyhwe-FVmmrK-lQ/viewform"
+    },
+
+    eco27: {
+        title: "Экономический рост",
+        lesson: "27",
+        notes: "27. Экономический рост.pdf",
+        homework: "ДЗ 27. Экономический рост.docx",
+        test: "https://docs.google.com/forms/d/e/1FAIpQLScbbLfL7j7CUEOmhQNtD02yrbdX7u7yuLk65LGUJNQy6KmhLA/viewform"
+    },
+
+    eco28: {
+        title: "Налоги",
+        lesson: "28",
+        notes: "28. Налоги.pdf",
+        homework: "ДЗ 28. Налоги.docx",
+        test: "https://docs.google.com/forms/d/e/1FAIpQLSdA1SeKjXdQFbDfI3HMAlUyXc_XuH2EClaVRejI0WMIpjV8lQ/viewform"
     }
 
 };
